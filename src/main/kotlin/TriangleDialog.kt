99edddc89223725b8515f68d852b4d85d68a9f66
Влад Концевых <vladkontsevykh@list.ru>
2020-12-10 23:12:07 +0300

    Design!

--- src/main/kotlin/TriangleDialog.kt
+++ src/main/kotlin/TriangleDialog.kt
@@ -1,5 +1,10 @@
+import androidx.compose.desktop.AppWindowAmbient
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -7,8 +12,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.MenuBar
 
 @Composable
 fun TriangleDialog(onDismissFun: () -> Unit, onCreated: (MutableState<Triangle>) -> Unit) {
@@ -16,45 +23,62 @@ fun TriangleDialog(onDismissFun: () -> Unit, onCreated: (MutableState<Triangle>)
     val b = mutableStateOf(0.0)
     val c = mutableStateOf(0.0)
     val errorState = mutableStateOf(false)
+    val isFirst = mutableStateOf(true)
     val curA = remember { mutableStateOf("") }
     val curB = remember { mutableStateOf("") }
     val curC = remember { mutableStateOf("") }
     Dialog(onDismissRequest = onDismissFun) {
-        Column(Modifier.fillMaxSize().padding(top = 5.dp), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally) {
-            if (errorState.value)
-                Text("Such a triangle can't exist", modifier = Modifier.padding(5.dp))
-            Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(5.dp)) {
-                DataInputRow("a", curA.value, onValueChange = { str ->
-                    run {
-                        curA.value = str
-                        errorState.value = false
-                        a.value = str.toDoubleOrNull() ?: 0.0
-                    }
-                })
-                DataInputRow("b", curB.value, onValueChange = { str ->
-                    run {
-                        curB.value = str
-                        errorState.value = false
-                        b.value = str.toDoubleOrNull() ?: 0.0
-                    }
-                })
-                DataInputRow("c", curC.value, onValueChange = { str ->
-                    run {
-                        curC.value = str
-                        errorState.value = false
-                        c.value = str.toDoubleOrNull() ?: 0.0
-                    }
-                })
+        MaterialTheme(shapes = Shapes(RoundedCornerShape(0.dp), RoundedCornerShape(0.dp), RoundedCornerShape(0.dp)),
+                colors = MaterialTheme.colors.copy(primary = Color(80, 50, 50),
+                        onPrimary = Color.Black)
+        ) {
+            if (isFirst.value) {
+                AppWindowAmbient.current?.setTitle("Well, triangle")
+                AppWindowAmbient.current?.setSize(600, 400)
+                isFirst.value = false
             }
-            Button(modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
-                    onClick = {
-                        try {
-                            onCreated(mutableStateOf(Triangle(a.value, b.value, c.value)))
-                        } catch (e: Exception) {
-                            errorState.value = true
-                        }
-                    }) {
-                Text("Create")
+            Box(Modifier.background(Color(80, 80, 80))) {
+                Column(Modifier.fillMaxSize().padding(top = 5.dp),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                    if (errorState.value)
+                        Text("Such a triangle can't exist", modifier = Modifier.padding(5.dp))
+                    else
+                        Box(Modifier.preferredHeight(28.dp)) // Precision magic!
+                    Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                        DataInputRow("a", curA.value, onValueChange = { str ->
+                            run {
+                                curA.value = str
+                                errorState.value = false
+                                a.value = str.toDoubleOrNull() ?: 0.0
+                            }
+                        })
+                        DataInputRow("b", curB.value, onValueChange = { str ->
+                            run {
+                                curB.value = str
+                                errorState.value = false
+                                b.value = str.toDoubleOrNull() ?: 0.0
+                            }
+                        })
+                        DataInputRow("c", curC.value, onValueChange = { str ->
+                            run {
+                                curC.value = str
+                                errorState.value = false
+                                c.value = str.toDoubleOrNull() ?: 0.0
+                            }
+                        })
+                    }
+                    Button(modifier = Modifier.preferredWidth(200.dp).padding(top = 5.dp, bottom = 5.dp),
+                            onClick = {
+                                try {
+                                    onCreated(mutableStateOf(Triangle(a.value, b.value, c.value)))
+                                } catch (e: Exception) {
+                                    errorState.value = true
+                                }
+                            }) {
+                        Text("Create")
+                    }
+                }
             }
         }
     }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,12 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Close
 import androidx.compose.material.icons.sharp.KeyboardArrowDown
 import androidx.compose.material.icons.sharp.KeyboardArrowUp
 import androidx.compose.runtime.Composable
@@ -20,79 +21,88 @@ import androidx.compose.ui.unit.dp
 
 fun main() = Window(title = "Well, figures") {
     val list = arrayListOf(Circle(24.0), Triangle(10.0, 5.0, 6.0), Rectangle(20.0, 10.0), Square(25.0))
-    MaterialTheme {
-        Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            val summonTriangleDialog = remember { mutableStateOf(false) }
-            Column(Modifier.fillMaxHeight(), Arrangement.spacedBy(5.dp, Alignment.Top)) {
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp)
-                    .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 25.dp),
-                    onClick = {}) {
-                    Text("Удалить")
-                }
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp)
-                    .padding(start = 10.dp, end = 10.dp),
-                    onClick = { summonTriangleDialog.value = true }) {
-                    Text("Добавить треугольник")
-                }
-                if (summonTriangleDialog.value) {
-                    var retVal: MutableState<Triangle>
-                    TriangleDialog(
-                        onDismissFun = { summonTriangleDialog.value = false; println("That's how we do") },
-                        onCreated = { returned -> retVal = returned; summonTriangleDialog.value = false; list.add(retVal.value)})
-                }
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp)
-                    .padding(start = 10.dp, end = 10.dp),
-                    onClick = {}) {
-                    Text("Добавить прямоугольник")
-                }
-                Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp)
-                    .padding(start = 10.dp, end = 10.dp),
-                    onClick = {}) {
-                    Text("Добавить круг")
-                }
-                Button(
-                    modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp)
-                        .padding(start = 10.dp, end = 10.dp),
-                    onClick = {}) {
-                    Text("Добавить квадрат")
-                }
-            }
-
-            Box(
-                modifier = Modifier.fillMaxHeight()
-                    .background(color = Color(180, 180, 180))
-                    .padding(10.dp)
-            ) {
-                val stateVertical = rememberScrollState(0f)
-                val stateHorizontal = rememberScrollState(0f)
-
-                ScrollableColumn(
-                    modifier = Modifier.fillMaxHeight(),
-                    scrollState = stateVertical
-                ) {
-                    Column {
-                        for (item in list) {
-                            ListElement("$item")
-                        }
+    MaterialTheme(shapes = Shapes(RoundedCornerShape(0.dp), RoundedCornerShape(0.dp), RoundedCornerShape(0.dp)),
+            colors = MaterialTheme.colors.copy(primary = Color(80,50,50),
+            onPrimary = Color.Black)
+    ) {
+        Box(Modifier.background(Color(80, 80, 80))) {
+            Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                val summonTriangleDialog = remember { mutableStateOf(false) }
+                Column(Modifier.fillMaxHeight(), Arrangement.spacedBy(0.dp, Alignment.Top)) {
+                    Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp),
+                            onClick = { summonTriangleDialog.value = true }) {
+                        Text("Добавить треугольник")
+                    }
+                    if (summonTriangleDialog.value) {
+                        var retVal: MutableState<Triangle>
+                        TriangleDialog(
+                                onDismissFun = { summonTriangleDialog.value = false },
+                                onCreated = { returned -> retVal = returned; summonTriangleDialog.value = false; list.add(retVal.value) })
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp),
+                            onClick = {}) {
+                        Text("Добавить прямоугольник")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp),
+                            onClick = {}) {
+                        Text("Добавить круг")
+                    }
+                    Button(
+                            modifier = Modifier.align(Alignment.CenterHorizontally).width(300.dp),
+                            onClick = {}) {
+                        Text("Добавить квадрат")
                     }
                 }
-                VerticalScrollbar(
-                    modifier = Modifier.align(Alignment.CenterEnd)
-                        .fillMaxHeight(),
-                    adapter = rememberScrollbarAdapter(stateVertical)
-                )
+
+                Box(
+                        modifier = Modifier.fillMaxHeight()
+                                .background(color = Color(120, 120, 120))
+                                .padding(10.dp)
+                ) {
+                    val stateVertical = rememberScrollState(0f)
+                    val stateHorizontal = rememberScrollState(0f)
+
+                    ScrollableColumn(
+                            modifier = Modifier.fillMaxHeight(),
+                            scrollState = stateVertical
+                    ) {
+                        Column {
+                            for (i in list.indices) {
+                                ListElement("${list[i]}",
+                                        {list.removeAt(i)},
+                                        {},
+                                        {})
+                            }
+                        }
+                    }
+                    VerticalScrollbar(
+                            modifier = Modifier.align(Alignment.CenterEnd)
+                                    .fillMaxHeight(),
+                            adapter = rememberScrollbarAdapter(stateVertical)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun ListElement(string: String) =
-    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-        Text(text = string, color = Color.Blue, textAlign = TextAlign.Start)
-        Row(modifier = Modifier.padding(end = 10.dp), horizontalArrangement = Arrangement.End) {
-            Icon(Icons.Sharp.KeyboardArrowUp, Modifier.clickable { })
-            Icon(Icons.Sharp.KeyboardArrowDown, Modifier.clickable { })
+fun ListElement(string: String,
+                onDeleteClick: () -> Unit,
+                onUpClick: () -> Unit,
+                onDownClick: () -> Unit) =
+        Row(horizontalArrangement = Arrangement.Start,
+        modifier = Modifier.padding(top = 2.dp, bottom = 2.dp)
+                .border(BorderStroke(2.dp, Color.Black), RoundedCornerShape(25))
+        ) {
+            Icon(Icons.Sharp.Close, Modifier.clickable {  })
+            Row(horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = string, textAlign = TextAlign.Start, modifier = Modifier.padding(start = 5.dp))
+                Row(modifier = Modifier.padding(end = 10.dp), horizontalArrangement = Arrangement.End) {
+                    Icon(Icons.Sharp.KeyboardArrowUp, Modifier.clickable { })
+                    Icon(Icons.Sharp.KeyboardArrowDown, Modifier.clickable { })
+                }
+            }
         }
-    }
-
